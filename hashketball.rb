3aa2752cd57b87aede4cfd9d4571d0eb09fded15
David Jun <djun301@gmail.com>
2021-02-16 18:45:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-        "Alan Anderson" => {
+      :players =>[ 
+            {:player_name => "Alan Anderson",
             :number => 0,
             :shoe => 16,
             :points => 22,
@@ -15,7 +15,7 @@ def game_hash
             :blocks => 1,
             :slam_dunks => 1
         },
-        "Reggie Evans" => {
+            {:player_name => "Reggie Evans",
             :number => 30,
             :shoe => 14,
             :points => 12,
@@ -25,7 +25,7 @@ def game_hash
             :blocks => 12,
             :slam_dunks => 7
         },
-        "Brook Lopez" => {
+            {:player_name => "Brook Lopez",
             :number => 11,
             :shoe => 17,
             :points => 17,
@@ -35,7 +35,7 @@ def game_hash
             :blocks => 1,
             :slam_dunks => 15
         },
-        "Mason Plumlee" => {
+            {:player_name => "Mason Plumlee",
             :number => 1,
             :shoe => 19,
             :points => 26,
@@ -45,7 +45,7 @@ def game_hash
             :blocks => 8,
             :slam_dunks => 5
         },
-        "Jason Terry" => {
+            {:player_name => "Jason Terry",
             :number => 31,
             :shoe => 15,
             :points => 19,
@@ -55,7 +55,7 @@ def game_hash
             :blocks => 11,
             :slam_dunks => 1
         }
-      }
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
