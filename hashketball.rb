107d8ab55d11536d420df612c08f7a8ef949cbab
David Jun <djun301@gmail.com>
2021-02-16 18:54:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,7 +61,7 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => {
-            {:player_name => "Jeff Adrien",
+        "Jeff Adrien" => {
             :number => 4,
             :shoe => 18,
             :points =>  10,
@@ -71,7 +71,7 @@ def game_hash
             :blocks => 7,
             :slam_dunks => 2
         },
-            {:player_name => "Bismak Biyombo",
+        "Bismak Biyombo" => {
             :number => 0,
             :shoe => 16,
             :points => 12,
@@ -81,7 +81,7 @@ def game_hash
             :blocks => 15,
             :slam_dunks => 10
         },
-            {:player_name => "DeSagna Diop",
+        "DeSagna Diop" => {
             :number => 2,
             :shoe => 14,
             :points => 24,
@@ -91,7 +91,7 @@ def game_hash
             :blocks => 5,
             :slam_dunks => 5
         },
-            {:player_name => "Ben Gordon",
+        "Ben Gordon" => {
             :number => 8,
             :shoe => 15,
             :points => 33,
@@ -101,7 +101,7 @@ def game_hash
             :blocks => 1,
             :slam_dunks => 0
         },
-            {:player_name => "Brendan Haywood",
+        "Brendan Haywood" => {
             :number => 33,
             :shoe => 15,
             :points => 6,
@@ -111,7 +111,6 @@ def game_hash
             :blocks => 5,
             :slam_dunks => 12
         }
-      end
       }
     }
   }
